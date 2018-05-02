3b2f9e51734cf5b13bdc4a90dccea9cfb69935b0
objkevin <objkevin@gmail.com>
2018-05-02 13:16:57 -0500

    conexión
    conexión a la base de datos

---
+++ src/Conectarse.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conectarse {
+    private Connection conn = null;
+    private String host = "danielbenito.com";
+    private String db = "itlac_genesysmi";
+    private String user = "itlac_kevin";
+    private String pass = "1,FWwSff;}%0";
+
+    public Conectarse() {
+        MySQLAccess();
+    }
+    
+    public void MySQLAccess() {
+        try {
+            // This will load the MySQL driver, each DB has its own driver
+            Class.forName("com.mysql.jdbc.Driver");
+            // Setup the connection with the DB
+            conn = DriverManager.getConnection("jdbc:mysql://"+host+"/"+db,user,pass);
+            // Check the state of connection
+            if (conn!=null){
+            System.out.println("Conexión a la base de datos "+db+" OK");
+            }
+        } catch (Exception e) {
+            System.out.println("error en la conexión a la base de datos\n");
+            System.out.println(e);  
+        }
+    }
+}
